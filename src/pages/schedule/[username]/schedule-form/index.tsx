bb392b15a6016660c3ce46e15da67992fa65a836
André Silveira <andre.silveira1silva@gmail.com>
2024-10-01 07:16:32 -0300

    feat: add Schedule form and Confirm Schedule.

---
+++ src/pages/schedule/[username]/schedule-form/index.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+import { CalendarStep } from './calendar-step'
+import { ConfirmStep } from './confirm-step'
+
+export function ScheduleForm() {
+  const [selectedDateTime, setSelectedDateTime] = useState<Date | null>()
+
+  function handleClearSelectedDateTime() {
+    setSelectedDateTime(null)
+  }
+
+  if (selectedDateTime) {
+    return (
+      <ConfirmStep
+        schedulingDate={selectedDateTime}
+        onCancelConfirmation={handleClearSelectedDateTime}
+      />
+    )
+  }
+
+  return <CalendarStep onSelectDateTime={setSelectedDateTime} />
+}
